a7ecec87db67170dc29a6a14bc135f1ac534cc0b
Hardik <69380034+Hardikkukadiya36@users.noreply.github.com>
2025-11-05 13:57:39 +0530

    Update base URL in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = 'https://123movies.com'; // Replace with your actual domain
+  const baseUrl = 'https://123moviesfreestreaming.com'; // Replace with your actual domain
 
   return {
     rules: [
